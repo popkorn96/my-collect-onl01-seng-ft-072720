296ff5f2738268337338fb126d15ed416e8c7a03
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-31 14:29:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(languages)
+  i = 0
+  lang = []
+  while i < languages.length 
+  lang << yield(languages[i])
+  i += i + 1
+end
+lang
+end
 
-
+def my_collect(students)
+  i = 0
+  stud = []
+  while i < students.length 
+  stud << yield(students[i])
+  i += i + 1
+end
+lang
+end
