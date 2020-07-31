fb829e539189a3fca5392fe728f9354cb048928f
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-07-31 14:30:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
   i = 0
   new_collection = []
   while i < languages.length 
-  new_collection << yield(languages[i])
+  new_collection.upcase << yield(languages[i])
   i += i + 1
 end
 new_collection
